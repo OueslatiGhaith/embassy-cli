9e25c4a6b9d360d9f5399a1a53f9aa4529b7df86
GhaithOueslati <ghaith.oueslati@enis.tn>
2023-08-13 18:35:21 +0100

    initial commit

---
+++ src/generate.rs
@@ -0,0 +1,30 @@
+use cargo_generate::{GenerateArgs, TemplatePath};
+
+pub struct GeneratorConfig {
+    pub name: String,
+    pub vendor: String,
+    pub mcu: String,
+    pub target: String,
+}
+
+pub fn create(cfg: GeneratorConfig) -> anyhow::Result<()> {
+    let args = GenerateArgs {
+        template_path: TemplatePath {
+            path: Some("./template".to_owned()),
+            ..Default::default()
+        },
+        silent: true,
+        verbose: true,
+        name: Some(cfg.name),
+        define: vec![
+            format!("vendor={}", cfg.vendor),
+            format!("mcu={}", cfg.mcu),
+            format!("target={}", cfg.target),
+        ],
+        ..Default::default()
+    };
+
+    cargo_generate::generate(args)?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use data::DATA;
+use generate::{create, GeneratorConfig};
+
+mod data;
+mod generate;
+
+fn main() -> anyhow::Result<()> {
+    let name = inquire::Text::new("Project name").prompt()?;
+    let vendor = inquire::Select::new("Select a vendor", DATA.vendor_list()).prompt()?;
+    let mcu = inquire::Select::new("Select a MCU", DATA.mcu_list(&vendor)).prompt()?;
+    let target = DATA.target(&mcu);
+
+    let config = GeneratorConfig {
+        name,
+        vendor,
+        mcu,
+        target,
+    };
+    create(config)?;
+
+    Ok(())
+}
