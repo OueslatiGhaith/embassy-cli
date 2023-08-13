56850e63a2e5687f569f5904be689ceb2f0daeb9
GhaithOueslati <ghaith.oueslati@enis.tn>
2023-08-13 18:37:25 +0100

    fix: typos

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod generate;
 fn main() -> anyhow::Result<()> {
     let name = inquire::Text::new("Project name").prompt()?;
     let vendor = inquire::Select::new("Select a vendor", DATA.vendor_list()).prompt()?;
-    let mcu = inquire::Select::new("Select a MCU", DATA.mcu_list(&vendor)).prompt()?;
+    let mcu = inquire::Select::new("Select an MCU", DATA.mcu_list(&vendor)).prompt()?;
     let target = DATA.target(&mcu);
 
     let config = GeneratorConfig {
