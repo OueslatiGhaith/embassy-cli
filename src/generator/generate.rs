a8442b1bfff32a48e7e06205f771d6b19cb43088
GhaithOueslati <ghaith.oueslati@enis.tn>
2023-08-13 23:09:05 +0100

    dev: seperate between dev and prod templates

--- src/generator/generate.rs
+++ src/generator/generate.rs
@@ -8,12 +8,21 @@ pub struct GeneratorConfig {
 }
 
 pub fn create(cfg: GeneratorConfig) -> anyhow::Result<()> {
+    #[cfg(debug_assertions)]
+    let template_path = TemplatePath {
+        path: Some("./template".to_owned()),
+        ..Default::default()
+    };
+
+    #[cfg(not(debug_assertions))]
+    let template_path = TemplatePath {
+        git: Some("https://github.com/OueslatiGhaith/embassy-cli".to_owned()),
+        subfolder: Some("./template".to_owned()),
+        ..Default::default()
+    };
+
     let args = GenerateArgs {
-        template_path: TemplatePath {
-            git: Some("https://github.com/OueslatiGhaith/embassy-cli".to_owned()),
-            subfolder: Some("./template".to_owned()),
-            ..Default::default()
-        },
+        template_path,
         silent: true,
         verbose: true,
         name: Some(cfg.name),
