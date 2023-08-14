ea2da0a1cad5776b26395446b6a6a65c49b0df6c
GhaithOueslati <ghaith.oueslati@enis.tn>
2023-08-14 13:54:07 +0100

    feat: format generated files

--- src/generator/generate.rs
+++ src/generator/generate.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use cargo_generate::{GenerateArgs, TemplatePath};
 
 pub struct GeneratorConfig {
@@ -34,7 +36,37 @@ pub fn create(cfg: GeneratorConfig) -> anyhow::Result<()> {
         ..Default::default()
     };
 
-    cargo_generate::generate(args)?;
+    let path = cargo_generate::generate(args)?;
+
+    // run cargo fmt
+    std::process::Command::new("cargo")
+        .arg("fmt")
+        .current_dir(&path)
+        .output()?;
+
+    // format toml files
+    let toml_paths = [path.join("Cargo.toml"), path.join("./.cargo/config.toml")];
+    for toml_path in toml_paths {
+        let toml = std::fs::read_to_string(&toml_path)?;
+        let mut toml = toml.parse::<toml_edit::Document>()?;
+        toml.as_table_mut().fmt();
+
+        let as_string = toml.to_string();
+        let mut file = std::fs::File::create(toml_path)?;
+        let as_string = remove_triple_newlines(&as_string);
+        file.write_all(as_string.as_bytes())?;
+    }
 
     Ok(())
+}
+
+fn remove_triple_newlines(string: &str) -> String {
+    let mut new_string = String::new();
+    for char in string.chars() {
+        if char == '\n' && new_string.ends_with("\n\n") {
+            continue;
+        }
+        new_string.push(char);
+    }
+    new_string
 }
